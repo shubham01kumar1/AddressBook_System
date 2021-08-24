8b6496a1c5b2bdaa03fcacb4bcadc9a9007844de
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-24 13:12:53 +0530

    Greeting message

---
+++ AddressController/AddressController.java
@@ -0,0 +1,8 @@
+package AddressController;
+
+public class AddressController {
+
+    public static void main(String[] args ){
+        System.out.println("Welcome to AddrssBook problem");
+    }
+}
